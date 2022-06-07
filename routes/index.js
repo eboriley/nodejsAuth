e3254789facce73fdde794903e5a15da3ddd41f1
Emmanuel Sackey <eboriley@hotmail.co.uk>
2022-06-07 23:49:26 +0000

    express session working for backend

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
 const userController = require('../controllers/users');
+const session = require('express-session');
 
 router.get('/', (req, res) => {
-  res.send('Hello eboriley!');
+  return res.json('Hello eboriley!');
 });
 
 router.post('/login', userController.login);
+
+router.get(
+  '/people',
+  userController.isAuth,
+  userController.adminEvents
+);
+
+router.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
 
 router.post('/register', userController.addUser);
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,42 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/index');
+const MySQLStore = require('express-mysql-session')(session);
 const app = express();
 const port = 5001;
 
+const options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'auth',
+  createDatabaseTable: true,
+  schema: {
+    tableName: 'sessions_table',
+    columnNames: {
+      session_id: 'session_id',
+      expires: 'expires',
+      data: 'data',
+    },
+  },
+};
+
+const sessionStore = new MySQLStore(options);
+
 if (app.get('env') === 'production') {
   app.set('trust proxy', 1); // trust first proxy
-  session.cookie.seure = true; //serve secure session
+  session.cookie.secure = true; //serve secure session
 }
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -17,7 +45,8 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true },
+    store: sessionStore,
+    cookie: { secure: false },
     // logic to save session to db
   })
 );
